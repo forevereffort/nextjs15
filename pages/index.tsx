3bac29e9243f6c0e7427d0f22fa97950cc7a6ba1
forevereffort <forevereffort@yandex.com>
2022-12-26 22:50:43 +0800

    work

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,45 @@ const Home: NextPage = () => {
             </div>
             <nav className={HeaderStyles.nav}>
               <ul>
-                <li><a href="#" className={HeaderStyles.hasMegaMenu}>Why 15Five</a></li>
+                <li>
+                  <a href="#" className={HeaderStyles.hasMegaMenu}>Why 15Five</a>
+                  <div className={HeaderStyles.megaNav}>
+                    <div className={BaseStyles.container}>
+                      <div className={HeaderStyles.megaNavRow}>
+                        <div className={HeaderStyles.megaNavLeft}>
+                          <ul class="navbar__submenu">                                                    
+                            <li class="navbar__submenu__item">
+                              <a className={HeaderStyles.subLink} href="#">
+                                <span className={HeaderStyles.subTitle}>Why Choose 15Five?</span>
+                                <div className={HeaderStyles.subDes}>Our approach to people is different - and we're proud of it. </div>
+                              </a>
+                            </li>                                                 
+                            <li class="navbar__submenu__item">
+                              <a className={HeaderStyles.subLink} href="#">
+                                <span className={HeaderStyles.subTitle}>Customer Stories</span>
+                                <div className={HeaderStyles.subDes}>Hear why our customers love us in their words</div>
+                              </a>
+                            </li>                                                   
+                            <li class="navbar__submenu__item">
+                              <a className={HeaderStyles.subLink} href="#">
+                                <span className={HeaderStyles.subTitle}>Best-Self Philosophy</span>
+                                <div className={HeaderStyles.subDes}>How we drive performance from the inside out</div>
+                              </a>
+                            </li>                                                
+                            <li class="navbar__submenu__item">
+                              <a className={HeaderStyles.subLink} href="#">
+                                <span className={HeaderStyles.subTitle}>Community</span>
+                                <div className={HeaderStyles.subDes}>Join the movement of HR Superstars</div>
+                              </a>
+                            </li>
+                          </ul>
+                        </div>
+                        <div className={HeaderStyles.megaNavRight}>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
                 <li><a href="#" className={HeaderStyles.hasMegaMenu}>Products</a></li>
                 <li><a href="#" className={HeaderStyles.hasMegaMenu}>Resources</a></li>
                 <li><a href="#" className={HeaderStyles.hasMegaMenu}>Company</a></li>
