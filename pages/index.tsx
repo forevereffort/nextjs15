26f8123b01cf2b165e9eb2ea187349bcdaabc370
forevereffort <forevereffort@yandex.com>
2022-12-28 21:41:11 +0800

    work

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,27 +26,27 @@ const Home: NextPage = () => {
                   <div className={HeaderStyles.megaNav}>
                     <div className={BaseStyles.container}>
                       <div className={HeaderStyles.megaNavRow}>
-                        <div className={HeaderStyles.megaNavLeft}>
-                          <ul class="navbar__submenu">                                                    
-                            <li class="navbar__submenu__item">
+                        <div className={`${HeaderStyles.megaNavLeft} ${HeaderStyles.megaTwoCols}`}>
+                          <ul className={HeaderStyles.subMenu}>
+                            <li className={HeaderStyles.subMenuItem}>
                               <a className={HeaderStyles.subLink} href="#">
                                 <span className={HeaderStyles.subTitle}>Why Choose 15Five?</span>
                                 <div className={HeaderStyles.subDes}>Our approach to people is different - and we're proud of it. </div>
                               </a>
                             </li>                                                 
-                            <li class="navbar__submenu__item">
+                            <li className={HeaderStyles.subMenuItem}>
                               <a className={HeaderStyles.subLink} href="#">
                                 <span className={HeaderStyles.subTitle}>Customer Stories</span>
                                 <div className={HeaderStyles.subDes}>Hear why our customers love us in their words</div>
                               </a>
                             </li>                                                   
-                            <li class="navbar__submenu__item">
+                            <li className={HeaderStyles.subMenuItem}>
                               <a className={HeaderStyles.subLink} href="#">
                                 <span className={HeaderStyles.subTitle}>Best-Self Philosophy</span>
                                 <div className={HeaderStyles.subDes}>How we drive performance from the inside out</div>
                               </a>
                             </li>                                                
-                            <li class="navbar__submenu__item">
+                            <li className={HeaderStyles.subMenuItem}>
                               <a className={HeaderStyles.subLink} href="#">
                                 <span className={HeaderStyles.subTitle}>Community</span>
                                 <div className={HeaderStyles.subDes}>Join the movement of HR Superstars</div>
@@ -55,6 +55,37 @@ const Home: NextPage = () => {
                           </ul>
                         </div>
                         <div className={HeaderStyles.megaNavRight}>
+                          <div className={HeaderStyles.lead}>
+                            <div className={HeaderStyles.title}>Solutions</div>
+                            <div className={HeaderStyles.description}>Your path to success in building high-performing organizations</div>
+                          </div>
+                          <ul>
+                            <li className={HeaderStyles.subMenuItem}>
+                              <a className={HeaderStyles.subLink} href="#">
+                                <span className={HeaderStyles.subTitle}>Increasing Engagement</span>
+                              </a>
+                            </li>                                                 
+                            <li className={HeaderStyles.subMenuItem}>
+                              <a className={HeaderStyles.subLink} href="#">
+                                <span className={HeaderStyles.subTitle}>Improving Manager Effectiveness</span>
+                              </a>
+                            </li>                                                   
+                            <li className={HeaderStyles.subMenuItem}>
+                              <a className={HeaderStyles.subLink} href="#">
+                                <span className={HeaderStyles.subTitle}>Remote Team Development</span>
+                              </a>
+                            </li>                                                
+                            <li className={HeaderStyles.subMenuItem}>
+                              <a className={HeaderStyles.subLink} href="#">
+                                <span className={HeaderStyles.subTitle}>Aligning Company Success</span>
+                              </a>
+                            </li>
+                            <li className={HeaderStyles.subMenuItem}>
+                              <a className={HeaderStyles.subLink} href="#">
+                                <span className={HeaderStyles.subTitle}>People Development</span>
+                              </a>
+                            </li>
+                          </ul>
                         </div>
                       </div>
                     </div>
