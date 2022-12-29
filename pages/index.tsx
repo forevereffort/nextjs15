a3344e6b3c10c9339abec4be7e51cca48b55e1e7
forevereffort <forevereffort@yandex.com>
2022-12-29 15:27:04 +0800

    work

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
+      <Header/>
     </>
   )
 }
